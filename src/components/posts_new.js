4977f2a61d0c47b0cb35f88151a9c73d18971a4e
Jason Leong <soundslikejason@users.noreply.github.com>
2017-03-02 12:01:23 -0800

    form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -12,6 +12,9 @@ class PostsNew extends Component {
         <div className="form-group">
           <label>Title</label>
           <input type="type" className="form-control" {...title} />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
         <div className="form-group">
@@ -30,10 +33,21 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a username';
+  }
+
+  return errors;
+}
+
 // connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
 // reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
 
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
